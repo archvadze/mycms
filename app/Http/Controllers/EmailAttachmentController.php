a55b32a917dae94ba66b42959558110ada6273aa
Besarion Archvadze <contact@acumencraft.com>
2026-08-08 17:40:38 +0400

    Improve email attachment download diagnostics

--- app/Http/Controllers/EmailAttachmentController.php
+++ app/Http/Controllers/EmailAttachmentController.php
@@ -18,20 +18,29 @@ class EmailAttachmentController extends Controller
                 'Admin',
                 'Support',
             ]),
-            403
+            403,
+            'Not authorized to download email attachments.'
         );
 
         $attachment = collect($message->attachments ?? [])
             ->firstWhere('id', $attachmentId);
 
-        abort_unless($attachment, 404);
+        abort_unless(
+            $attachment,
+            404,
+            'Attachment metadata not found.'
+        );
 
         $resendEmailId = data_get(
             $message->metadata,
             'resend_email_id'
         );
 
-        abort_unless($resendEmailId, 404);
+        abort_unless(
+            $resendEmailId,
+            404,
+            'Resend email ID not found.'
+        );
 
         $remoteAttachment = $resend
             ->emails
@@ -44,7 +53,8 @@ class EmailAttachmentController extends Controller
 
         abort_unless(
             ! empty($remoteAttachment->download_url),
-            404
+            404,
+            'Resend download URL not found.'
         );
 
         return redirect()->away(
